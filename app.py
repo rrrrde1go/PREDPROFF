9675a5cc60f23ce016a3e2131c13dd248cfd9ee3
rrrrde1go <rrrrde1go@gmail.com>
2026-03-15 10:50:48 +0300

    добавление render template для домашней страницы

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Главная страница</h1>'
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
