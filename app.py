8913045ae09ae01b36db344d5aed508bc63ad41b
rrrrde1go <rrrrde1go@gmail.com>
2026-03-15 11:14:19 +0300

    Добавление понятной маршрутизации

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+
 @app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
+
+@app.route('/graph')
+def graph():
+    return render_template('graph.html')
 
 
 if __name__ == "__main__":
