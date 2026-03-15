b2eff3ac596cd0845527b8701378b75a1c28f60c
rrrrde1go <rrrrde1go@gmail.com>
2026-03-15 11:47:40 +0300

    Логин и регистрация в header

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
