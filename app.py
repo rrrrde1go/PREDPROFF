7f5454f17d35ab164737cc78861a89236b657897
n3shura <verevkinsashenka@gmail.com>
2026-03-15 12:15:20 +0300

    Добавил БД

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key = True)
+    name = db.Column("Name", db.String(45))
+    surname = db.Column("Surname", db.String(45))
+    login = db.Column("login", db.String(45), nullable = False, unique = True)
+    password = db.Column('password', db.String(40))
 
 @app.route('/')
 @app.route('/index')
