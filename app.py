f997b4fd6d6aa030f21b168af0150e9cae8ee347
rrrrde1go <rrrrde1go@gmail.com>
2026-03-15 10:47:55 +0300

    Улучшение структуры; Добавление главной страницы;

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return '<h1>Главная страница</h1>'
+
+
 if __name__ == "__main__":
     app.run(debug=True)
